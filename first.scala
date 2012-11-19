7e9f6f2d3814092b0f2d419a519fb8aa15bf9c5a
Mike Plavsky <Mike.Plavsky@quest.com>
2012-11-19 22:42:18 +0000

    prettier

--- first.scala
+++ first.scala
@@ -1,4 +1,4 @@
 case class Player(first:String,last:String)
 
 def singleExpression: List[String] => (List[Int], List[Int]) = 
-    { a => a map (_.toInt) partition (_ < 30)}
+    {a => a map (_.toInt) partition (_ < 30)}
