8adf24067042cfc14534e6b5175a8d9bbf6f0df8
Mike Plavsky <Mike.Plavsky@quest.com>
2012-11-19 22:47:20 +0000

    more examples

--- first.scala
+++ first.scala
@@ -1,4 +1,9 @@
 case class Player(first:String,last:String)
 
+var two = Player("Marry", "Jane")
+var one = Player(first="John", last="Mac")
+
 def singleExpression: List[String] => (List[Int], List[Int]) = 
     {a => a map (_.toInt) partition (_ < 30)}
+
+singleExpression( List("34","50", "20", "70" ))
