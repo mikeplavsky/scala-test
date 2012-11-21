51a0e0e9ac4d46d399ebc329d21970dd0d049284
Mike Plavsky <Mike.Plavsky@quest.com>
2012-11-21 21:59:43 +0000

    trait

--- first.scala
+++ first.scala
@@ -11,12 +11,27 @@ def singleExpression: List[String] => (List[Int], List[Int]) =
 
 singleExpression( List("34","50", "20", "70" ))
 
-case class Square(var side: Int) {
+trait Maker {
+  def make = ""
+}
+
+case class Square(var side: Int) extends Maker {
   def area = side * side
 }
 
 val a = Square(12)
 a.area
+a.make
 
 val b = Square(5)
 b.area
+
+trait CheckIt extends Maker {
+  override def make = "Done"
+}
+
+var p = new Square(5) with CheckIt
+p.make
+
+
+
