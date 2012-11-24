6cb3b6af4bbc39bd9f317945431dfe8974c5dd5d
Mike Plavsky <Mike.Plavsky@quest.com>
2012-11-24 22:55:20 +0000

    style

--- second.scala
+++ second.scala
@@ -9,4 +9,4 @@ val less5 = {less(_:Int, 5)}
 val more5 = {not[Int](less5)(_)}
 
 def curry[A,B,C](f: (A,B) => C): A => B => C = {x:A => {f(x,_)}}
-def uncurry[A,B,C](f: A=>B=>C): (A,B)=>C = {(x:A,y:B) => f(x)(y)}
+def uncurry[A,B,C](f: A => B => C): (A,B) => C = {(x:A,y:B) => f(x)(y)}
