bde9dbe20332ebf47a4cd7545b0dbfae348265fd
Mike Plavsky <Mike.Plavsky@quest.com>
2012-11-24 22:53:46 +0000

    currying

--- second.scala
+++ second.scala
@@ -8,3 +8,5 @@ def not[A](p: Pred[A]):Pred[A] = a => !p(a)
 val less5 = {less(_:Int, 5)}
 val more5 = {not[Int](less5)(_)}
 
+def curry[A,B,C](f: (A,B) => C): A => B => C = {x:A => {f(x,_)}}
+def uncurry[A,B,C](f: A=>B=>C): (A,B)=>C = {(x:A,y:B) => f(x)(y)}
